a10a48aa8654394001fc099808df03786d8cba4c
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-08-25 16:07:34 +0300

    commit tree 5f2722c06e1c589168948339276dfe9bdcff5a71

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr.Chocolate", 45, "M", "Black", "Happy", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy", 25, "M", "Black", "Angry", False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha", 5, "F", "White", "Moody", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 25, "F", "Black", "Happy", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa", 35, "F", "Brown", "Mad", False);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch", 18, "M", "Green", "Grumpy", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy", 19, "F", "Brown", "Angry", True);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL, 85, "M", "White", "Happy", False);
